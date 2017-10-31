e9abb9b9c13a58dfdd8f24b65cb3d1a7aa07c334
Situ William <situ_william@yahoo.com>
2017-10-31 14:38:45 -0400

    added most of the exercises

---
+++ exercise4.rb
@@ -0,0 +1,9 @@
+def greet_backwards(your_name)
+	reversed = your_name.reverse
+	puts("Hello, #{reversed+reversed}! Welcome home.")
+end
+
+greet_backwards("Bob")
+greet_backwards("Shirly")
+greet_backwards("Sue")
+greet_backwards("Andy")
